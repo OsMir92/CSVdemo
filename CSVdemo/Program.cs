203645eaa42cbc07a0f1f933d92f15e4499d170d
Oscar Roberto Miranda Espinoza <rob.mir.92@outlook.es>
2020-12-11 12:24:21 -0800

    Adding README and rest of classes

--- CSVdemo/Program.cs
+++ CSVdemo/Program.cs
@@ -6,7 +6,36 @@ namespace CSVdemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\n\n");
+            //Write sample data to a .csv file
+            using (CsvFileWriter writer = new CsvFileWriter("Test.csv"))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    CsvRow row = new CsvRow();
+                    for (int j = 0; j < 3; j++)
+                        row.Add(String.Format("Column{0}", j));
+                    writer.WriteRow(row);
+                }
+            }
+            
+            //Read the sample data
+            using (CsvFileReader reader = new CsvFileReader("Test.csv"))
+            {
+                CsvRow row = new CsvRow();
+                while (reader.ReadRow(row))
+                {
+                    foreach (string s in row)
+                    {
+                        Console.Write(s);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
         }
+
     }
+
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+*Demo for CSV file write and read*
+
+** Got the demo from: https://www.codeproject.com/Articles/415732/Reading-and-Writing-CSV-Files-in-Csharp **
